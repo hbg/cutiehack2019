7facd6b658cda395d895fe70952aba80ecb3a0cb
John Shin <jshin029@ucr.edu>
2019-10-05 23:24:05 -0700

    Updates

---
+++ precache-manifest.96c953a26a87255449334f974127ed0f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42ee1789203f29756a2a8f9637412abe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "08b88b0f65fd1894fa15",
+    "url": "/static/css/2.802837ff.chunk.css"
+  },
+  {
+    "revision": "cccde085120f3193f369",
+    "url": "/static/css/main.f39d561d.chunk.css"
+  },
+  {
+    "revision": "08b88b0f65fd1894fa15",
+    "url": "/static/js/2.06615128.chunk.js"
+  },
+  {
+    "revision": "cccde085120f3193f369",
+    "url": "/static/js/main.3fd3f3c8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "293d03e011c15c6d549cef03385d6621",
+    "url": "/static/media/Icon.293d03e0.png"
+  },
+  {
+    "revision": "0859f69657d194e3967d6fe0e486035d",
+    "url": "/static/media/acm.0859f696.png"
+  },
+  {
+    "revision": "f65e5715cd00e07f5cfebc19139f40bc",
+    "url": "/static/media/cutieFooter.f65e5715.png"
+  },
+  {
+    "revision": "e595e38e862ed5152a7db6e0be03d905",
+    "url": "/static/media/cutieHome.e595e38e.png"
+  },
+  {
+    "revision": "bc966731074282cea7b048057e33b8af",
+    "url": "/static/media/hero.bc966731.png"
+  }
+]);
