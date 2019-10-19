cf056a5054dfbc067063f40b0e0dde398465c8b6
Jthai006 <32521845+Jthai006@users.noreply.github.com>
2019-10-19 11:50:33 -0700

    created navbar for desktop and mobile version

--- src/components/Live.js
+++ src/components/Live.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import LiveNavbar from './LiveNavbar'
+import './css/Live.css'
 
 class Live extends Component {
   state = {
@@ -9,9 +11,18 @@ class Live extends Component {
   render(){
     return(
       <div>
-        hello
+
+        <LiveNavbar></LiveNavbar>
+        <div className="display-1">
+          <div id = "schedule">THIS IS PAGE 111111111</div>
+          <div id = "schedule1">THIS IS PAGE 222222222</div>
+          <div id = "schedule2">THIS IS PAGE 333333333</div>
+          <div id = "schedule3">THIS IS PAGE 444444444</div>
+          <div id = "schedule4">THIS IS PAGE 555555555</div>
       </div>
-    )
+      </div>
+
+    );
   }
 
 
